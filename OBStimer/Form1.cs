51621e2ea537f40861bdc6de8a0b96084a5c1e34
explorer7 <victorcalvo12@gmail.com>
2015-11-19 16:52:53 -0600

    0.25

---
+++ OBStimer/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OBStimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                button1.Text = "Stop";
+                timer1.Enabled = false;
+            }
+            else if (timer1.Enabled == false)
+            {
+                button1.Text = "Start";
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = timer1.
+        }
+    }
+}
