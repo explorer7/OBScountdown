93dcf05613f9963c917c739945600254ae29a5a7
explorer7 <victorcalvo12@gmail.com>
2015-11-24 09:03:06 -0600

    .75 Alpha

--- OBStimer/Form1.Designer.cs
+++ OBStimer/Form1.Designer.cs
@@ -29,23 +29,19 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.hourTB = new System.Windows.Forms.TextBox();
+            this.minuteLabel = new System.Windows.Forms.Label();
+            this.minuteTB = new System.Windows.Forms.TextBox();
+            this.secondTB = new System.Windows.Forms.TextBox();
+            this.secondLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "timer";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 58);
+            this.button1.Location = new System.Drawing.Point(77, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -58,25 +54,92 @@
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // hourTB
+            // 
+            this.hourTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourTB.Location = new System.Drawing.Point(41, 42);
+            this.hourTB.MaxLength = 2;
+            this.hourTB.Name = "hourTB";
+            this.hourTB.Size = new System.Drawing.Size(30, 31);
+            this.hourTB.TabIndex = 2;
+            this.hourTB.Text = "00";
+            this.hourTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hourTB.TextChanged += new System.EventHandler(this.hourTB_TextChanged);
+            this.hourTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.hourTB_KeyPress);
+            // 
+            // minuteLabel
+            // 
+            this.minuteLabel.AutoSize = true;
+            this.minuteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteLabel.Location = new System.Drawing.Point(77, 45);
+            this.minuteLabel.Name = "minuteLabel";
+            this.minuteLabel.Size = new System.Drawing.Size(18, 25);
+            this.minuteLabel.TabIndex = 3;
+            this.minuteLabel.Text = ":";
+            // 
+            // minuteTB
+            // 
+            this.minuteTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteTB.Location = new System.Drawing.Point(101, 42);
+            this.minuteTB.MaxLength = 2;
+            this.minuteTB.Name = "minuteTB";
+            this.minuteTB.Size = new System.Drawing.Size(30, 31);
+            this.minuteTB.TabIndex = 4;
+            this.minuteTB.Text = "00";
+            this.minuteTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.minuteTB.TextChanged += new System.EventHandler(this.minuteTB_TextChanged);
+            this.minuteTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.minuteTB_KeyPress);
+            // 
+            // secondTB
+            // 
+            this.secondTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondTB.Location = new System.Drawing.Point(161, 42);
+            this.secondTB.MaxLength = 2;
+            this.secondTB.Name = "secondTB";
+            this.secondTB.Size = new System.Drawing.Size(30, 31);
+            this.secondTB.TabIndex = 5;
+            this.secondTB.Text = "00";
+            this.secondTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.secondTB.TextChanged += new System.EventHandler(this.secondTB_TextChanged);
+            this.secondTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.secondTB_KeyPress);
+            // 
+            // secondLabel
+            // 
+            this.secondLabel.AutoSize = true;
+            this.secondLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel.Location = new System.Drawing.Point(137, 45);
+            this.secondLabel.Name = "secondLabel";
+            this.secondLabel.Size = new System.Drawing.Size(18, 25);
+            this.secondLabel.TabIndex = 6;
+            this.secondLabel.Text = ":";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(127, 100);
+            this.ClientSize = new System.Drawing.Size(233, 145);
+            this.Controls.Add(this.secondLabel);
+            this.Controls.Add(this.secondTB);
+            this.Controls.Add(this.minuteTB);
+            this.Controls.Add(this.minuteLabel);
+            this.Controls.Add(this.hourTB);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "OBS Timer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox hourTB;
+        private System.Windows.Forms.Label minuteLabel;
+        private System.Windows.Forms.TextBox minuteTB;
+        private System.Windows.Forms.TextBox secondTB;
+        private System.Windows.Forms.Label secondLabel;
     }
 }
 
--- OBStimer/Form1.cs
+++ OBStimer/Form1.cs
@@ -12,28 +12,109 @@ namespace OBStimer
 {
     public partial class Form1 : Form
     {
+
+        public static int hour, minute, second, temp, temp2, total;
+        public static string text;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void hourTB_TextChanged(object sender, EventArgs e)
+        {
+            hour = Convert.ToInt32(hourTB.Text);
+        }
+
+        private void minuteTB_TextChanged(object sender, EventArgs e)
+        {
+            minute = Convert.ToInt32(minuteTB.Text);
+        }
+
+        private void hourTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void minuteTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void secondTB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void secondTB_TextChanged(object sender, EventArgs e)
+        {
+            second = Convert.ToInt32(secondTB.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (timer1.Enabled == true)
             {
-                button1.Text = "Stop";
+                button1.Text = "Start";
                 timer1.Enabled = false;
+                hourTB.ReadOnly = false;
+                minuteTB.ReadOnly = false;
+                secondTB.ReadOnly = false;
+                hourTB.BackColor = Color.White;
+                minuteTB.BackColor = Color.White;
+                secondTB.BackColor = Color.White;
             }
             else if (timer1.Enabled == false)
             {
-                button1.Text = "Start";
+                button1.Text = "Stop";
+                convertToSeconds();
                 timer1.Enabled = true;
+                hourTB.ReadOnly = true;
+                minuteTB.ReadOnly = true;
+                secondTB.ReadOnly = true;
+                hourTB.BackColor = Color.Turquoise;
+                minuteTB.BackColor = Color.Turquoise;
+                secondTB.BackColor = Color.Turquoise;
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = timer1.
+            if (total >= 0) { 
+                total -= 1;
+                temp = total % 3600;
+                temp2 = total / 3600;
+                hourTB.Text = Convert.ToString(temp2);
+                text += Convert.ToString(temp2);
+                text += " : ";
+                temp2 = temp / 60;
+                minuteTB.Text = Convert.ToString(temp2);
+                text += Convert.ToString(temp2);
+                text += " : ";
+                temp = temp % 60;
+                secondTB.Text = Convert.ToString(temp);
+                text += Convert.ToString(temp);
+                System.IO.File.WriteAllText(@"countdown.txt", text);
+                text = "";
+            } else
+            {
+                hourTB.BackColor = Color.IndianRed;
+                minuteTB.BackColor = Color.IndianRed;
+                secondTB.BackColor = Color.IndianRed;
+            }
+        }
+        public static void convertToSeconds()
+        {
+            total = (hour * 3600) + (minute * 60) + second;
         }
     }
 }
